70955cfc41f4c74c5a427101ffc9cb0c3f1afc2e
maskerliu <lynx.chrisliu@gmail.com>
2021-03-30 14:29:53 +0800

    remove game func files

--- src/main/index.ts
+++ src/main/index.ts
@@ -2,7 +2,6 @@ import { app, BrowserWindow, Menu, nativeImage, shell, Tray } from "electron"
 import path from "path"
 // import AsarUpdateService from "./AsarUpdateService"
 import LocalServer from "./LocalServer"
-import GameFrameSyncService from "./GameFrameSyncService"
 require("./IPCService")
 
 
@@ -110,7 +109,6 @@ app.on("ready", () => {
   // AsarUpdateService.setParentWindow(mainWindow);
   // AsarUpdateService.check();
   LocalServer.startProxyHttpServer();
-  GameFrameSyncService.start();
 });
 
 app.on("window-all-closed", () => {
